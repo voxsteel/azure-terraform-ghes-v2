50ed19cf8148d97b97bbf1fe85f318888141084a
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-01-03 16:07:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,26 +80,21 @@ resource "azurerm_virtual_machine" "main" {
     disk_size_gb      = "200"
   }
 
- # os_profile {
- #   computer_name  = "hostname"
- #   admin_username = var.linux_admin_username
- # }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.linux_admin_username
+  }
 
-#  os_profile_linux_config {
-#    disable_password_authentication = true
-#    ssh_keys {
-#      path     = "/home/voxsteel/.ssh/authorized_keys"
-#      key_data = var.ssh_public_key
-#    }
-  computer_name  = "${var.prefix}-vm"
-  admin_username = var.linux_admin_username
-  disable_password_authentication = true
+  os_profile_linux_config {
+    disable_password_authentication = true
   
-  admin_ssh_key {
-        username       = var.linux_admin_username
-        public_key     = file("~/.ssh/unused/id_rsa.pub")
+    ssh_keys {
+      path     = "/home/voxsteel/.ssh/authorized_keys"
+      key_data = var.ssh_public_key
     }
   }
+}
+
 
 
 resource "azurerm_managed_disk" "main" {
