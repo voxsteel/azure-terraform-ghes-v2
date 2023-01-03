278192742af80793ddfe8c383629d0fdc58023b9
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-01-03 16:26:44 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   value       = azurerm_public_ip.main.ip_address
   description = "The IP address of the GitHub Enterprise Server instance"
 }
+
+output "VM-admin" {
+    description = "Username for Virtual Machine administrator account is:"
+    value = var.linux_admin_username
+}
