ed2febb8e9adaf0272671f7516fa2cb6ca0a7cfe
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-01-03 15:50:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,12 +100,12 @@ resource "azurerm_virtual_machine" "main" {
 
 
 resource "azurerm_managed_disk" "main" {
-  name                 = "${var.prefix}-data.vhd"
+  name                 = "${var.prefix}-data-disk"
   location             = azurerm_resource_group.main.location
   resource_group_name  = azurerm_resource_group.main.name
   storage_account_type = "Premium_LRS"
   create_option        = "Empty"
-  disk_size_gb         = 175
+  disk_size_gb         = 200
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "main" {
