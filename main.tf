18f23207ae16f8c8fa9d58195fb2bd8472b1e540
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-01-03 15:45:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,17 +85,17 @@ resource "azurerm_virtual_machine" "main" {
     admin_username = var.linux_admin_username
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/voxsteel/.ssh/authorized_keys"
-      key_data = var.ssh_public_key
-    }
-
-#  admin_ssh_key {
-#        username       = var.linux_admin_username
-#        public_key     = file("~/.ssh/unused/id_rsa.pub")
+#  os_profile_linux_config {
+#    disable_password_authentication = true
+#    ssh_keys {
+#      path     = "/home/voxsteel/.ssh/authorized_keys"
+#      key_data = var.ssh_public_key
 #    }
+
+  admin_ssh_key {
+        username       = var.linux_admin_username
+        public_key     = file("~/.ssh/unused/id_rsa.pub")
+    }
   }
 }
 
