0602caad6bced8fb5def2d07802baf54aa22a191
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-01-04 08:28:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_virtual_machine" "main" {
   location              = azurerm_resource_group.main.location
   resource_group_name   = azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS11_v2"
+  vm_size               = "Standard_E8-4ads_v5"
 
   storage_image_reference {
     publisher = "GitHub"
