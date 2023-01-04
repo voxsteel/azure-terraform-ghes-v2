61564558a42b4ce44fa7164d730e4904f5943c62
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-01-04 08:33:07 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "public_ip" {
-  value       = azurerm_public_ip.main.ip_address
   description = "The IP address of the GitHub Enterprise Server instance"
+  value       = azurerm_public_ip.main.ip_address
 }
 
 output "VM-admin" {
